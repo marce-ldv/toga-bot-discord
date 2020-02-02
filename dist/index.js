3e4a2b2b7afd2214cbdcba96f46b7cd3101ffb3b
MarceloLudovino <ext-m.ludovino@rappi.com>
2020-02-02 19:21:39 -0300

    add kick members with auth and clean all messages

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,53 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 dotenv_1.config();
 const discord_js_1 = require("discord.js");
+const config_json_1 = require("./config/config.json");
 const client = new discord_js_1.Client();
 client.on('ready', () => {
     console.log('toga is here');
 });
+client.on('message', (msg) => __awaiter(void 0, void 0, void 0, function* () {
+    // console.log(msg);
+    if (msg.content.startsWith(config_json_1.prefix)) {
+        // msg.channel.send(`❄️ Watashi wa kita`);
+        // msg.reply(`Your the best`);
+    }
+    if (msg.content.startsWith(`${config_json_1.prefix}hi`)) {
+        const member = msg.mentions.members.first();
+        console.log(member);
+        if (member) {
+            msg.channel.send(`Hi ${member}`);
+        }
+    }
+    if (msg.content.startsWith(`${config_json_1.prefix}kick`)) {
+        const member = msg.mentions.members.first();
+        if (member && msg.member.hasPermission(['KICK_MEMBERS'])) {
+            const removedMember = yield msg.member.kick();
+            const { username } = removedMember.user;
+            msg.reply(`${username} has been removed from this channel`);
+        }
+        return msg.reply('You not have permissions for that action');
+    }
+    if (msg.content.startsWith(`${config_json_1.prefix}cleanAll`)) {
+        try {
+            const mesagges = yield msg.channel.fetchMessages();
+            yield msg.channel.bulkDelete(mesagges);
+        }
+        catch (err) {
+            console.log(err);
+            msg.reply('You not have permissions for that action');
+        }
+    }
+}));
 client.login(process.env.TOGA_TOKEN);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,51 @@
 import { config } from 'dotenv';
 config();
-import { Client } from 'discord.js';
+import { Client, Message } from 'discord.js';
+import { prefix } from './config/config.json';
 
 const client: Client = new Client();
 
 client.on('ready', () => {
   console.log('toga is here');
 });
+
+client.on('message', async (msg: Message) => {
+  // console.log(msg);
+
+  if(msg.content.startsWith(prefix)){
+    // msg.channel.send(`❄️ Watashi wa kita`);
+    // msg.reply(`Your the best`);
+  }
+
+  if(msg.content.startsWith(`${prefix}hi`)){
+    const member = msg.mentions.members.first();
+    console.log(member);
+    if(member){
+      msg.channel.send(`Hi ${member}`)
+    }
+  }
+
+  if(msg.content.startsWith(`${prefix}kick`)){
+    const member = msg.mentions.members.first();
+
+    if(member && msg.member.hasPermission(['KICK_MEMBERS'])){
+      const removedMember = await msg.member.kick();
+      const { username } = removedMember.user;
+      msg.reply(`${username} has been removed from this channel`);
+    }
+    return msg.reply('You not have permissions for that action');
+  }
+
+  if(msg.content.startsWith(`${prefix}cleanAll`)){
+    try{
+      const mesagges = await msg.channel.fetchMessages();
+      await msg.channel.bulkDelete(mesagges);
+    }catch(err){
+      console.log(err);
+      msg.reply('You not have permissions for that action')
+    }
+    
+  }
+})
 
 client.login(process.env.TOGA_TOKEN);
